0076365c950d4607544d5a61fee3a4c210cc2c02
Tieppo <88698431+JTieppo@users.noreply.github.com>
2023-10-03 20:43:27 -0300

    .

--- main.c
+++ main.c
@@ -62,164 +62,169 @@ int main() {
     
     // Importa e carrega o arquivo da fonte  
     ALLEGRO_FONT *fonte = al_load_font("./assets/inter.ttf", 65, 0);
+
+    // Define caracteristicas
+    int espessura_sharp = 5;
+    int espessura_circulo = 4;
+    int espessura_retangulo = 4;
     
 
 
     // Desenha o tabuleiro
-    al_draw_line(300, 250, 300, 750, cor_linha, 5);
-    al_draw_line(600, 250, 600, 750, cor_linha, 5);
-    al_draw_line(200, 350, 700, 350, cor_linha, 5);
-    al_draw_line(200, 650, 700, 650, cor_linha, 5);
+    al_draw_line(300, 250, 300, 750, cor_linha, espessura_sharp);
+    al_draw_line(600, 250, 600, 750, cor_linha, espessura_sharp);
+    al_draw_line(200, 350, 700, 350, cor_linha, espessura_sharp);
+    al_draw_line(200, 650, 700, 650, cor_linha, espessura_sharp);
 
 
     // DESENHA OS CIRCULOS
     //superior esquerdo
-    al_draw_circle(200, 250, 30, cor_circulo, 4);
+    al_draw_circle(200, 250, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 210, 0, "1");
-    al_draw_circle(200, 250, 40, cor_circulo, 4);
+    al_draw_circle(200, 250, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 210, 0, "2");
-    al_draw_circle(200, 250, 50, cor_circulo, 4);
+    al_draw_circle(200, 250, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 210, 0, "3");
 
     //meio esquerdo
-    al_draw_circle(200, 490, 30, cor_circulo, 4);
+    al_draw_circle(200, 490, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 450, 0, "1");
-    al_draw_circle(200, 490, 40, cor_circulo, 4);
+    al_draw_circle(200, 490, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 450, 0, "2");
-    al_draw_circle(200, 490, 50, cor_circulo, 4);
+    al_draw_circle(200, 490, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 450, 0, "3");
 
     //inferior esquerdo
-    al_draw_circle(200, 750, 30, cor_circulo, 4);
+    al_draw_circle(200, 750, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 710, 0, "1");
-    al_draw_circle(200, 750, 40, cor_circulo, 4);
+    al_draw_circle(200, 750, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 710, 0, "2");
-    al_draw_circle(200, 750, 50, cor_circulo, 4);
+    al_draw_circle(200, 750, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 185, 710, 0, "3");
 
     //superior meio
-    al_draw_circle(450, 250, 30, cor_circulo, 4);
+    al_draw_circle(450, 250, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 210, 0, "1");
-    al_draw_circle(450, 250, 40, cor_circulo, 4);
+    al_draw_circle(450, 250, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 210, 0, "2");
-    al_draw_circle(450, 250, 50, cor_circulo, 4);
+    al_draw_circle(450, 250, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 210, 0, "3");
 
     //meio meio
-    al_draw_circle(450, 490, 30, cor_circulo, 4);
+    al_draw_circle(450, 490, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 450, 0, "1");
-    al_draw_circle(450, 490, 40, cor_circulo, 4);
+    al_draw_circle(450, 490, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 450, 0, "2");
-    al_draw_circle(450, 490, 50, cor_circulo, 4);
+    al_draw_circle(450, 490, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 450, 0, "3");
 
     //inferior meio
-    al_draw_circle(450, 750, 30, cor_circulo, 4);
+    al_draw_circle(450, 750, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 710, 0, "1");
-    al_draw_circle(450, 750, 40, cor_circulo, 4);
+    al_draw_circle(450, 750, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 710, 0, "2");
-    al_draw_circle(450, 750, 50, cor_circulo, 4);
+    al_draw_circle(450, 750, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 430, 710, 0, "3");
 
     //superior direito
-    al_draw_circle(700, 250, 30, cor_circulo, 4);
+    al_draw_circle(700, 250, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 210, 0, "1");
-    al_draw_circle(700, 250, 40, cor_circulo, 4);
+    al_draw_circle(700, 250, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 210, 0, "2");
-    al_draw_circle(700, 250, 50, cor_circulo, 4);
+    al_draw_circle(700, 250, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 210, 0, "3");
 
     //meio direito
-    al_draw_circle(700, 490, 30, cor_circulo, 4);
+    al_draw_circle(700, 490, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 450, 0, "1");
-    al_draw_circle(700, 490, 40, cor_circulo, 4);
+    al_draw_circle(700, 490, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 450, 0, "2");
-    al_draw_circle(700, 490, 50, cor_circulo, 4);
+    al_draw_circle(700, 490, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 450, 0, "3");
 
     //inferior direito
-    al_draw_circle(700, 750, 30, cor_circulo, 4);
+    al_draw_circle(700, 750, 30, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 710, 0, "1");
-    al_draw_circle(700, 750, 40, cor_circulo, 4);
+    al_draw_circle(700, 750, 40, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 710, 0, "2");
-    al_draw_circle(700, 750, 50, cor_circulo, 4);
+    al_draw_circle(700, 750, 50, cor_circulo, espessura_circulo);
     al_draw_text(fonte, cor_circulo, 680, 710, 0, "3");
     // FIM CIRCULOS
 
 
     // DESENHA OS RETANGULOS
     //superior esquerdo
-    al_draw_rectangle(150, 200, 250, 300, cor_retangulo, 4);
+    al_draw_rectangle(150, 200, 250, 300, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 210, 0, "1");
-    al_draw_rectangle(160, 210, 240, 290, cor_retangulo, 4);
+    al_draw_rectangle(160, 210, 240, 290, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 210, 0, "2");
-    al_draw_rectangle(170, 220, 230, 280, cor_retangulo, 4);
+    al_draw_rectangle(170, 220, 230, 280, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 210, 0, "3");
 
     //meio esquerdo
-    al_draw_rectangle(150, 440, 250, 540, cor_retangulo, 4);
+    al_draw_rectangle(150, 440, 250, 540, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 450, 0, "1");
-    al_draw_rectangle(160, 450, 240, 530, cor_retangulo, 4);
+    al_draw_rectangle(160, 450, 240, 530, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 450, 0, "2");
-    al_draw_rectangle(170, 460, 230, 520, cor_retangulo, 4);
+    al_draw_rectangle(170, 460, 230, 520, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 450, 0, "3");
 
     //inferior esquerdo
-    al_draw_rectangle(150, 700, 250, 800, cor_retangulo, 4);
+    al_draw_rectangle(150, 700, 250, 800, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 710, 0, "1");
-    al_draw_rectangle(160, 710, 240, 790, cor_retangulo, 4);
+    al_draw_rectangle(160, 710, 240, 790, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 710, 0, "2");
-    al_draw_rectangle(170, 720, 230, 780, cor_retangulo, 4);
+    al_draw_rectangle(170, 720, 230, 780, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 185, 710, 0, "3");
 
     //superior meio
-    al_draw_rectangle(400, 200, 500, 300, cor_retangulo, 4);
+    al_draw_rectangle(400, 200, 500, 300, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 210, 0, "1");
-    al_draw_rectangle(410, 210, 490, 290, cor_retangulo, 4);
+    al_draw_rectangle(410, 210, 490, 290, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 210, 0, "2");
-    al_draw_rectangle(420, 220, 480, 280, cor_retangulo, 4);
+    al_draw_rectangle(420, 220, 480, 280, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 210, 0, "3");
 
     //meio meio
-    al_draw_rectangle(400, 440, 500, 540, cor_retangulo, 4);
+    al_draw_rectangle(400, 440, 500, 540, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 450, 0, "1");
-    al_draw_rectangle(410, 450, 490, 530, cor_retangulo, 4);
+    al_draw_rectangle(410, 450, 490, 530, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 450, 0, "2");
-    al_draw_rectangle(420, 460, 480, 520, cor_retangulo, 4);
+    al_draw_rectangle(420, 460, 480, 520, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 450, 0, "3");
 
     //inferior meio
-    al_draw_rectangle(400, 700, 500, 800, cor_retangulo, 4);
+    al_draw_rectangle(400, 700, 500, 800, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 710, 0, "1");
-    al_draw_rectangle(410, 710, 490, 790, cor_retangulo, 4);
+    al_draw_rectangle(410, 710, 490, 790, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 710, 0, "2");
-    al_draw_rectangle(420, 720, 480, 780, cor_retangulo, 4);
+    al_draw_rectangle(420, 720, 480, 780, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 430, 710, 0, "3");
     
 
     //superior direito
-    al_draw_rectangle(650, 200, 750, 300, cor_retangulo, 4);
+    al_draw_rectangle(650, 200, 750, 300, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 210, 0, "1");
-    al_draw_rectangle(660, 210, 740, 290, cor_retangulo, 4);
+    al_draw_rectangle(660, 210, 740, 290, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 210, 0, "2");
-    al_draw_rectangle(670, 220, 730, 280, cor_retangulo, 4);
+    al_draw_rectangle(670, 220, 730, 280, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 210, 0, "3");
 
 
     //meio direito
-    al_draw_rectangle(650, 440, 750, 540, cor_retangulo, 4);
+    al_draw_rectangle(650, 440, 750, 540, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 450, 0, "1");
-    al_draw_rectangle(660, 450, 740, 530, cor_retangulo, 4);
+    al_draw_rectangle(660, 450, 740, 530, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 450, 0, "2");
-    al_draw_rectangle(670, 460, 730, 520, cor_retangulo, 4);
+    al_draw_rectangle(670, 460, 730, 520, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 450, 0, "3");
 
     //inferior direito
-    al_draw_rectangle(650, 700, 750, 800, cor_retangulo, 4);
+    al_draw_rectangle(650, 700, 750, 800, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 710, 0, "1");
-    al_draw_rectangle(660, 710, 740, 790, cor_retangulo, 4);
+    al_draw_rectangle(660, 710, 740, 790, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 710, 0, "2");
-    al_draw_rectangle(670, 720, 730, 780, cor_retangulo, 4);
+    al_draw_rectangle(670, 720, 730, 780, cor_retangulo, espessura_retangulo);
     al_draw_text(fonte, cor_retangulo, 680, 710, 0, "3");
     // FIM RETANGULOS
 
